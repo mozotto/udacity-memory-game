a648cecb485dd0ea1e52ed03458c4243011a8c08
mozotto <emmanuelababio@gmail.com>
2024-06-28 11:03:31 +0000

    Add game score logic

--- js/app.js
+++ js/app.js
@@ -1,95 +1,33 @@
 (function () {
-    class MemoryGame {
-        /** The symbols displayed on the cards */
-        static #symbols = ['⚽', '⚾', '🏀', '🏈', '🏐', '🥎', '🥭', '🍌'];
+    /** The symbols displayed on the cards */
+    const symbols = ['⚽', '⚾', '🏀', '🏈', '🏐', '🥎', '🥭', '🍌'];
+    const cards = [];
+    let selectedCard; // the currently selected face up card
+    let score = 0;
 
-        /** 
-         * holds the cards that are still left to play 
-         * */
-        #remainingCards
-
-        constructor() {
-            this.#remainingCards = this.#createNewDeck();
-            this.#shuffle();
-        }
-
-        /** shuffles the cards in place */
-        #shuffle() {
-            const array = this.#remainingCards;
-            for (let i = (array.length - 1); i > 0; i--) { // countdown instead to avoid having to adjust for the lower bound of random
-                const j = Math.floor(Math.random() * (i + 1)); // randomly choose another card index
-                [array[j], array[i]] = [array[i], array[j]]; // swap those the cards at those 2 positions
-            }
-        }
-
-        #createNewDeck() {
-            return MemoryGame.#symbols.flatMap(symbol => [new Card(symbol), new Card(symbol)]);
-        }
-
-        /**
-         * Returns the deck of cards
-         * @returns {Array<Card>}
-         */
-        get cards() {
-            return this.#remainingCards;
-        }
-    }
-
-    class Card {
-        #isFaceUp = false;
-        #symbol
-
-        /**
-         * Create a new Card
-         * @param {string} symbol - The symbol displayed when the card is up
-         */
-        constructor(symbol) {
-            this.#symbol = symbol;
-        }
-
-        /** Turn the card face up */
-        turnUp() {
-            this.#isFaceUp = true;
-        }
-
-        /** Turn the card face down */
-        turnDown() {
-            this.#isFaceUp = false;
-        }
-
-        /**
-         * Check if the card is facing up
-         * @returns {boolean}
-         */
-        get isFaceUp() {
-            return this.#isFaceUp;
-        }
-
-        /**
-         * Get the symbol shown when card is face up
-         * @returns {string}
-         */
-        get symbol() {
-            return this.#symbol;
-        }
-    }
-
-    const grid = document.querySelector('#grid');
-    const game = new MemoryGame();
-    let selected;
-
-    /** Initializes the grid of cards */
-    function setUpGrid() {
-        const docFragment = new DocumentFragment(); // used to hold all the li elements
-        game.cards.forEach(card => {
-            const li = document.createElement('li');
-            li.card = card;
-            li.addEventListener('click', cardClickHandler);
-            const span = document.createElement('span');
-            li.append(span);
-            docFragment.appendChild(li);
+    /** create a new shuffled deck of cards */
+    function createNewDeck() {
+        symbols.forEach(symbol => {
+            const card1 = createCard(symbol);
+            const card2 = createCard(symbol);
+            cards.push(card1, card2);
         });
-        grid.appendChild(docFragment); // attach the list items to the ul
+
+        // shuffle cards
+        for (let i = (cards.length - 1); i > 0; i--) { // countdown instead to avoid having to adjust for the lower bound of random
+            const j = Math.floor(Math.random() * (i + 1)); // randomly choose another card index
+            [cards[j], cards[i]] = [cards[i], cards[j]]; // swap those the cards at those 2 positions
+        }
+        const grid = document.querySelector('#grid'); // card grid
+        grid.append(...cards); // attach the list items to the ul
+
+        function createCard(symbol) {
+            const card = document.createElement('li');
+            card.classList.add('card');
+            card.symbol = symbol; // attach
+            card.addEventListener('click', cardClickHandler);
+            return card
+        }
     }
 
     /**
@@ -97,29 +35,60 @@
      * @param {Event} evt
      */
     function cardClickHandler(evt) {
-        const cardItem = evt.currentTarget;
-        const card = cardItem.card;
-        if (card.isFaceUp) {
-            card.turnDown();
+        const card = evt.currentTarget;
+        if (card.innerText.length) return; // clicked on card already face up. No Op
+        card.innerText = card.symbol; // turn the card up
+        if (selectedCard) {
+            checkForMatch(card);
         } else {
-            card.turnUp();
+            // no card selected
+            selectedCard = card; // enter into selection mode
         }
-        paintCard(cardItem);
     }
 
-    /**
-     * Turn a card face up
-     * @param {HTMLLIElement} cardItem
+    function checkForMatch(card) {
+        //todo do comparison
+        if (selectedCard.symbol === card.symbol) {
+            // match made
+            score++; // increase score
+            refreshScore();
+            hideCards(card)
+        } else {
+            // no match
+            score--; // reduce score
+            refreshScore();
+            turnOverCards(card);
+        }
+    }
+
+    /** Turn over the chosen cards
+     * @param {HTMLLIElement} card
      */
-    function paintCard(cardItem) {
-        if (cardItem.card.isFaceUp) {
-            cardItem.querySelector('span').innerText = cardItem.card.symbol;
-        } else {
-            cardItem.querySelector('span').innerText = '';
-        }
+    function turnOverCards(card) {
+        setTimeout(() => {
+            selectedCard.innerText = '';
+            card.innerText = '';
+            selectedCard = null; // remove selection
+        }, 500);
     }
 
-    setUpGrid();
+    /** hide the chosen cards
+     * @param {HTMLLIElement} card
+     */
+    function hideCards(card) {
+        setTimeout(() => {
+            selectedCard.style.visibility = 'hidden';
+            card.style.visibility = 'hidden';
+            selectedCard = null; // remove selection
+        }, 500);
+    }
 
+    function refreshScore() {
+        const elem = document.querySelector("#score");
+        elem.innerText = `Score: ${score}`;
+    }
+
+    createNewDeck();
+    refreshScore()
 
 })();
